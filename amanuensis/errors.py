5972a309f161d3ebe819489187285ad0f1dd43b1
Tim Van Baak <tim.vanbaak@gmail.com>
2020-01-18 12:19:42 -0800

    Add documentation to custom errors

--- amanuensis/errors.py
+++ amanuensis/errors.py
@@ -3,10 +3,21 @@ class AmanuensisError(Exception):
 	pass
 
 class MissingConfigError(AmanuensisError):
+	"""A config file is missing that was expected to be present"""
 	pass
 
 class MalformedConfigError(AmanuensisError):
+	"""A config file could not be read and parsed"""
 	pass
 
 class ReadOnlyError(AmanuensisError):
+	"""A config was edited in readonly mode"""
+	pass
+
+class InternalMisuseError(AmanuensisError):
+	"""An internal helper method was called wrongly"""
+	pass
+
+class IndexMismatchError(AmanuensisError):
+	"""An id was obtained from an index, but the object doesn't exist"""
 	pass
