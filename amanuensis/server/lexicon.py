221790b00e7e8e6899450ccf223c35f7a4407621
Tim Van Baak <tim.vanbaak@gmail.com>
2020-01-20 08:55:28 -0800

    Add skeleton for lexicon pages

--- amanuensis/server/lexicon.py
+++ amanuensis/server/lexicon.py
@@ -14,9 +14,25 @@ def get_bp():
 	"""Create a blueprint for lexicon pages"""
 	bp = Blueprint('lexicon', __name__, url_prefix='/lexicon/<name>')
 
+	@bp.route('/contents/', methods=['GET'])
+	@login_required
+	def contents(name):
+		return "Lexicon " + str(name)
+
+	@bp.route('/rules/', methods=['GET'])
+	@login_required
+	def rules(name):
+		return "Lexicon " + str(name)
+
 	@bp.route('/session/', methods=['GET'])
 	@login_required
 	def session(name):
+		lex = lexicon.LexiconModel.by(name=name)
+		return render_template('lexicon/session.html', lexicon=lex)
+
+	@bp.route('/statistics/', methods=['GET'])
+	@login_required
+	def stats(name):
 		return "Lexicon " + str(name)
 
 	return bp
