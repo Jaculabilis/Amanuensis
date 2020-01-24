0cb6e78752741a801981a1fb7c616e74e76cbed2
Tim Van Baak <tim.vanbaak@gmail.com>
2020-01-24 10:17:53 -0800

    Add more info in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as f:
+	long_desc = f.read()
+
 setup(
 	name='amanuensis',
 	version='0.0.1',
+	author='Tim Van Baak',
+	description='An application for playing Lexicon, the encyclopedia RPG',
+	long_description=long_desc,
+	long_description_content_type='text/markdown',
+	url='https://github.com/Jaculabilis/Amanuensis',
 	packages=find_packages(),
 	include_package_data=True,
 	zip_safe=True,
