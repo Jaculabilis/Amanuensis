5444a45d902cee1238a806c5d5bb285b9a54eef3
Tim Van Baak <tim.vanbaak@gmail.com>
2020-03-21 09:01:36 -0700

    Move parser.text to parser.tokenizer

--- amanuensis/parser/__init__.py
+++ amanuensis/parser/__init__.py
@@ -3,5 +3,5 @@ Module encapsulating all markdown parsing functionality
 """
 
 from amanuensis.parser.analyze import FeatureCounter
-from amanuensis.parser.text import parse_raw_markdown
+from amanuensis.parser.tokenizer import parse_raw_markdown
 from amanuensis.parser.render import PreviewHtmlRenderer
